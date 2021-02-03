1dad33c8918d30eaaadc047ed5430c0bc7cf30d9
DailyCode4u <dailycode4u@gmail.com>
2021-02-03 14:19:05 +0300

    Using lists

--- dictionaries.py
+++ dictionaries.py
@@ -1,2 +1,12 @@
+# Using dictionaries
+# Question: How many times does a vowel appear in a word?
 
+avowel = ('a,e,i,o,u')
+adictionary = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+aword = input('enter a word ')
 
+for letter in aword:
+    if letter in avowel:
+        adictionary[letter] += 1
+
+print(adictionary)
