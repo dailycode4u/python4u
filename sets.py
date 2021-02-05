4cda0660631e894181b198f3f258899278ed1797
DailyCode4u <dailycode4u@gmail.com>
2021-02-05 14:46:12 +0300

    Using dictionaries

--- sets.py
+++ sets.py
@@ -1,3 +1,18 @@
 #key characteristics of sets is duplicate values are forbidden
-sampleset={1,2,3,4,1,2,3,4,1,2,3,4}
-print(sampleset)
+
+sampleset={'a','e','i','o','u','5','3','3','5','a'}
+print(sampleset) #notice the lack of duplicates in the output
+
+word="dawn48qwer123"
+
+#Find items in the sampleset that are missing in the word
+d=sampleset.difference(set(word))
+print(d)
+
+#join two sets
+e=sampleset.union(set(word))
+print(e)
+
+#Find vowel in word
+f=sampleset.intersection(set(word))
+print(f)
