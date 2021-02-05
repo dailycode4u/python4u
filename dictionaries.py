f5a5df422751637e9f7087fb7b0915a81a8e7521
DailyCode4u <dailycode4u@gmail.com>
2021-02-05 09:44:08 +0300

    Using dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -10,8 +10,8 @@ for letter in aword:
     if letter in avowel:
         #ensures dictionary is always populated. No Key error exception. Using setdefault method
         adictionary.setdefault(letter, 0)
-
         adictionary[letter] += 1
+
         #using sorted and items method
 for key, result in sorted(adictionary.items()):
     if result != 0:
